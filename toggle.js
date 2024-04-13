57739690fc25a4a2b2996e78713de038fd4ff200
M3dicCookie <pred102@hotmail.com>
2024-04-13 12:24:22 -0700

    Create toggle.js

---
+++ toggle.js
@@ -0,0 +1,8 @@
+function toggleDetails(id) {
+    var details = document.getElementById(id);
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
