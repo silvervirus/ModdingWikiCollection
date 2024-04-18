365e446f2ccae218a86937cffaaced534d8b7cff
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2024-04-17 23:24:42 -0600

    Cleanup and simplify navbar a bit

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -4,45 +4,43 @@ export default function NavBar() : JSX.Element {
     return (
         <>
             <nav className="navbar">
-                <ul>
-                    <li><a href="./index.html">Home</a></li>
-                    <li>
-                        <a href="#">Subnautica</a>
-                        <ul>
-                            <li><a href="./snhelp.html">Install Instructions</a></li>
-                            <li><a href="./snlegacy.html">Legacy</a></li>
-                            <li><a href="#">LivingLarge</a></li>
-                            <li><a href="#">TestBuilds</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">BelowZero</a>
-                        <ul>
-                            <li><a href="./bzhelp.html">Install Instructions</a></li>
-                            <li><a href="./Belowzero.html">Legacy</a></li>
-                            <li><a href="#">Stable</a></li>
-                            <li><a href="#">TestBuilds</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Modding Resources</a>
-                        <ul>
-                            <li><a href="#">Legacy</a></li>
-                            <li><a href="#">Stable</a></li>
-                            <li><a href="#">Thunderkit</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Communities</a>
-                        <ul>
-                            <li><a href="#">Modding libraries</a></li>
-                            <li><a href="#">Modding Servers</a></li>
-                            <li><a href="#">Other Modding Resources</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <div><a href="./index.html">Home</a></div>
+                <div>
+                    <a href="#">Subnautica</a>
+                    <ul>
+                        <li><a href="./snhelp.html">Install Instructions</a></li>
+                        <li><a href="./snlegacy.html">Legacy</a></li>
+                        <li><a href="#">LivingLarge</a></li>
+                        <li><a href="#">TestBuilds</a></li>
+                        
+                    </ul>
+                </div>
+                <div>
+                    <a href="#">BelowZero</a>
+                    <ul>
+                        <li><a href="./bzhelp.html">Install Instructions</a></li>
+                        <li><a href="./Belowzero.html">Legacy</a></li>
+                        <li><a href="#">Stable</a></li>
+                        <li><a href="#">TestBuilds</a></li>
+                        
+                    </ul>
+                </div>
+                <div>
+                    <a href="#">Modding Resources</a>
+                    <ul>
+                        <li><a href="#">Legacy</a></li>
+                        <li><a href="#">Stable</a></li>
+                        <li><a href="#">Thunderkit</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="#">Communities</a>
+                    <ul>
+                        <li><a href="#">Modding libraries</a></li>
+                        <li><a href="#">Modding Servers</a></li>
+                        <li><a href="#">Other Modding Resources</a></li>
+                    </ul>
+                </div>
             </nav>
         </>
     );
