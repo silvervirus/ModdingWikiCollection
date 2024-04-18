21aa624cf65a78c120f9fb05f76822fdb11730e6
Jayden Kohlman <2236351+JKohlman@users.noreply.github.com>
2024-04-17 23:14:43 -0600

    Pull navigation bar out into it's own file

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -3,47 +3,7 @@ import './Home.css';
 export default function NavBar() : JSX.Element {
     return (
         <>
-            <nav className="navbar">
-                <ul>
-                    <li><a href="./">Home</a></li>
-                    <li>
-                        <a href="#">Subnautica</a>
-                        <ul>
-                            <li><a href="/snhelp.html">Install Instructions</a></li>
-                            <li><a href="./snlegacy.html">Legacy</a></li>
-                            <li><a href="#">LivingLarge</a></li>
-                            <li><a href="#">TestBuilds</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">BelowZero</a>
-                        <ul>
-                            <li><a href="/bzhelp.html">Install Instructions</a></li>
-                            <li><a href="/Belowzero.html">Legacy</a></li>
-                            <li><a href="#">Stable</a></li>
-                            <li><a href="#">TestBuilds</a></li>
-                            
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Modding Resources</a>
-                        <ul>
-                            <li><a href="#">Legacy</a></li>
-                            <li><a href="#">Stable</a></li>
-                            <li><a href="#">Thunderkit</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#">Communities</a>
-                        <ul>
-                            <li><a href="#">Modding libraries</a></li>
-                            <li><a href="#">Modding Servers</a></li>
-                            <li><a href="#">Other Modding Resources</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
+            <NavBar />
             <br/>
             <div className="container">
                 <h1>Welcome To the Mod Backup wiki</h1>
