8080a29d526e4022b7a910299083c233b645677f
M3dicCookie <pred102@hotmail.com>
2024-04-20 22:20:37 -0700

    moretesting

--- src/ModList/ModList.tsx
+++ src/ModList/ModList.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import ModCircle from '../ModCircle/ModCircle';
 import './ModList.css';
 import { useParams } from 'react-router-dom';
-import { UserData, User } from '../Data/UserData';
 
 interface Mod {
     modname: string;
@@ -14,26 +13,26 @@ interface Mod {
 }
 
 export default function ModList(): JSX.Element {
-    const { username } = useParams<{ username: string }>();
-    const [mods, setMods] = React.useState<Mod[]>([]);
+    const { game } = useParams<{ game: string }>();
+    const [mods, setMods] = useState<Mod[]>([]);
 
-    React.useEffect(() => {
-        fetch(`./${username}.json`)
+    useEffect(() => {
+        fetch(`./${game}.json`)
             .then(response => response.json())
             .then((data: { mods: Mod[] }) => setMods(data.mods))
             .catch(error => console.error('Error fetching mods:', error));
-    }, [username]);
+    }, [game]);
 
     return (
         <>
             <NavBar />
             <div className="container">
-                <h2>{username}'s Mods</h2>
+                <h2>{game} Modders</h2>
                 <div className="table">
                     <div className="tableRow">
                         {mods.map((mod, index) => (
                             <div key={index} className="circle-container">
-                                <ModCircle modname={mod.modname} onClick={() => console.log('Circle clicked')} />
+                                <ModCircle mod={mod} />
                             </div>
                         ))}
                     </div>
