6651d9a067f38ee927085a02a510d980ad98c9f9
M3dicCookie <pred102@hotmail.com>
2024-04-20 21:53:29 -0700

    more testing

--- src/ModList/ModList.tsx
+++ src/ModList/ModList.tsx
@@ -4,14 +4,19 @@ import './ModList.css';
 import { useParams } from 'react-router-dom';
 import { UserData } from '../Data/UserData';
 
+interface User {
+    username: string;
+    mods: Mod[];
+}
+
 export default function ModList(): JSX.Element {
     const { game, username } = useParams<{ game: string; username: string }>();
-    const usersList = UserData[game];
+    const usersList: User[] | undefined = UserData[game];
 
     if (!usersList)
         return <span>User list not defined for {game}</span>;
 
-    const user = usersList.find((user) => user.username === username);
+    const user = usersList.find((user: User) => user.username === username);
     if (!user)
         return <span>User not found</span>;
 
