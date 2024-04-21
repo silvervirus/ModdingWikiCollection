12f25267a4c9a617456b194a1f4de75d236db77a
M3dicCookie <pred102@hotmail.com>
2024-04-20 22:42:34 -0700

    back to normal

--- src/ModList/ModList.tsx
+++ src/ModList/ModList.tsx
@@ -1,36 +1,30 @@
-import { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
-import ModCircle from '../ModCircle/ModCircle';
+import Circles from '../Circles/Circles'; // Import Circle component from the correct path
 import './ModList.css';
 import { useParams } from 'react-router-dom';
-
-export interface Mod {
-    modname: string;
-    download: string;
-    image: string;
-    requirements: string[];
-    description: string;
-}
+import { UserData } from '../Data/UserData';
 
 export default function ModList(): JSX.Element {
-    const { username } = useParams<{ username: string }>();
-    const [mods, setMods] = useState<Mod[]>([]);
 
-    useEffect(() => {
-        fetch(`${username}.json`)
-            .then(response => response.json())
-            .then((data: { mods: Mod[] }) => setMods(data.mods))
-            .catch(error => console.error('Error fetching mods:', error));
-    }, [username]);
+    // Pulls data from the URL Route that we defined in our router
+    // /mods/:game creates a variable named game in location
+    const location = useParams();
+    const game = location.game!;
+    const usersList = UserData[game];
+
+    if (!usersList)
+        return <span>User list not defined for {game}</span>;
 
     return (
         <>
             <NavBar />
+           
             <div className="container">
-                <h2>{username} Mods</h2>
+                <h2>Subnautica {} Modders</h2>
                 <div className="table">
                     <div className="tableRow">
-                        <ModCircle mods={mods} />
+                        {/* Render Circle component with user data */}
+                        <Circles users={usersList} />
                     </div>
                 </div>
             </div>
