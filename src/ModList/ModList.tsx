2665ded6ec21ca4aa0cffc3ec845f3b4aea59cf6
M3dicCookie <pred102@hotmail.com>
2024-04-20 22:25:19 -0700

    back to normal

--- src/ModList/ModList.tsx
+++ src/ModList/ModList.tsx
@@ -1,41 +1,30 @@
-import React, { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
-import ModCircle from '../ModCircle/ModCircle';
+import Circles from '../Circles/Circles'; // Import Circle component from the correct path
 import './ModList.css';
 import { useParams } from 'react-router-dom';
-
-interface Mod {
-    modname: string;
-    download: string;
-    image: string;
-    requirements: string[];
-    description: string;
-}
+import { UserData } from '../Data/UserData';
 
 export default function ModList(): JSX.Element {
-    const { game } = useParams<{ game: string }>();
-    const [mods, setMods] = useState<Mod[]>([]);
 
-    useEffect(() => {
-        fetch(`./${game}.json`)
-            .then(response => response.json())
-            .then((data: { mods: Mod[] }) => setMods(data.mods))
-            .catch(error => console.error('Error fetching mods:', error));
-    }, [game]);
+    // Pulls data from the URL Route that we defined in our router
+    // /mods/:game creates a variable named game in location
+    const location = useParams();
+    const game = location.game!;
+    const usersList = UserData[game];
+
+    if (!usersList)
+        return <span>User list not defined for {game}</span>;
 
     return (
         <>
-        
             <NavBar />
+           
             <div className="container">
-                <h2>{game} Modders</h2>
+                <h2>Subnautica {} Modders</h2>
                 <div className="table">
                     <div className="tableRow">
-                        {mods.map((mod, index) => (
-                            <div key={index} className="circle-container">
-                                <ModCircle mod={mod} />
-                            </div>
-                        ))}
+                        {/* Render Circle component with user data */}
+                        <Circles users={usersList} />
                     </div>
                 </div>
             </div>
