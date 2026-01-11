1b7ed84af247d9a8c3af16b3acab4ee1ce8836ed
M3dicCookie <pred102@hotmail.com>
2026-01-11 12:45:13 -0800

    Update Home.tsx

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,18 +1,18 @@
+import React from 'react';
 import NavBar from '../NavBar/NavBar';
 import './Home.css';
 
 export default function Home(): JSX.Element {
-    return (
-        <>
-            <NavBar />
-            <br/>
-            <div className="container">
-                <h1>Welcome To the Mod Backup Wiki</h1>
-                <h2>
-                    With this wiki, we hope to backup links used on Submodica. 
-                    If anything happens to Submodica, we will have the backups and mods available to the community.
-                </h2>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1>Welcome To the Mod Backup Wiki</h1>
+        <h2>
+          With this wiki we are hoping to backup links used on Submodica so if anything happens to Submodica
+          we will have the backups and mods available to the community.
+        </h2>
+      </div>
+    </>
+  );
 }
