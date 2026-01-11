0c16bb064a1e8a5d7cd685972fea4aa7bbf22b51
M3dicCookie <pred102@hotmail.com>
2026-01-11 12:32:05 -0800

    Refactor ModderPage to use ModCircle component

--- src/ModCircle/ModderPage.tsx
+++ src/ModCircle/ModderPage.tsx
@@ -1,34 +1,20 @@
-import NavBar from '../NavBar/NavBar';
-import Circles from './ModCircle'; // Import Circle component from the correct path
-import './ModList.css';
 import { useParams } from 'react-router-dom';
-import { ModData } from '../Data/ModData';
-import { UserData } from '../Data/UserData';
+import NavBar from '../NavBar/NavBar';
+import ModCircle from './ModCircle';
+import './ModList.css';
 
 export default function ModderPage(): JSX.Element {
+    const { Modder } = useParams();
 
-    // Pulls data from the URL Route that we defined in our router
-    // /mods/:game creates a variable named game in location
-    const location = useParams();
-    const game = location.game!;
-    const Modder = location.Modder!;
-    const usersList = UserData[game];
-    const ModList = ModData[Modder];
-
-    if (!usersList)
-        return <span>Mod list not defined for {Modder}</span>;
+    if (!Modder) return <span>No modder selected</span>;
 
     return (
         <>
             <NavBar />
-           
             <div className="container">
-                <h2>Subnautica {game} Modders</h2>
-                <div className="table">
-                    <div className="tableRow">
-                        {/* Render Circle component with user data */}
-                        <Circles users={usersList} />
-                    </div>
+                <h2>{Modder}'s Mods</h2>
+                <div className="circle-container">
+                    <ModCircle modderName={Modder} />
                 </div>
             </div>
         </>
