7224ae98bc1ef753128d1d4caef0dd912d644877
M3dicCookie <pred102@hotmail.com>
2026-01-11 12:52:32 -0800

    Update ModCircle.tsx

--- src/ModCircle/ModCircle.tsx
+++ src/ModCircle/ModCircle.tsx
@@ -1,68 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './ModCircle.css';
 
 interface Mod {
-    modname: string;
-    download: string;
-    image: string;
-    requirements: string[];
-    description: string;
+  modname: string;
+  download: string;
+  image: string;
+  requirements: string[];
+  description: string;
 }
 
 interface ModCircleProps {
-    modderName: string;
+  modderName: string;
 }
 
 const ModCircle: React.FC<ModCircleProps> = ({ modderName }) => {
-    const [mods, setMods] = React.useState<Mod[]>([]);
+  const [mods, setMods] = useState<Mod[]>([]);
 
-    React.useEffect(() => {
-        fetch(`./${modderName}.json`)
-            .then((response) => response.json())
-            .then((data) => setMods(data.mods))
-            .catch((error) => console.error('Error fetching mods:', error));
-    }, [modderName]);
+  useEffect(() => {
+    fetch(`./${modderName}.json`)
+      .then((res) => res.json())
+      .then((data) => setMods(data.mods))
+      .catch((err) => console.error(err));
+  }, [modderName]);
 
-    return (
-        <>
-            {mods.map((mod, index) => (
-                <div key={index} className="circle-container">
-                    <input type="checkbox" id={`toggle${index}`} className="toggle-input" />
-                    <label htmlFor={`toggle${index}`} className="circle" title={mod.modname}>
-                        <img src={mod.image} alt={mod.modname} className="circle-photo" />
-                    </label>
-
-                    <div id={`details${index}`} className="details">
-                        <div className="text">
-                            <div className="mod-name-background">
-                                <span className="mod-name">{mod.modname}</span>
-                                <br />
-                                <a href={mod.download} target="_blank" rel="noopener noreferrer">Download</a>
-                            </div>
-
-                            <div className="mod-reg-background">
-                                <details>
-                                    <summary className="regsummary"><span className="mod-reg">Mod Requirements</span></summary>
-                                    <ul className="nested-ul">
-                                        {mod.requirements.map((requirement, i) => (
-                                            <li key={i}><span>{requirement}</span></li>
-                                        ))}
-                                    </ul>
-                                </details>
-                            </div>
-
-                            <div className="mod-dec-background">
-                                <details>
-                                    <summary className="decsummary"><span className="mod-dec">Mod Description</span></summary>
-                                    <div className="dec-background"><span className="dec">{mod.description}</span></div>
-                                </details>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      {mods.map((mod, index) => (
+        <div key={index} className="circle-container">
+          <input type="checkbox" id={`toggle${index}`} className="toggle-input" />
+          <label htmlFor={`toggle${index}`} className="circle" title={mod.modname}>
+            <img src={mod.image} alt={mod.modname} className="circle-photo" />
+          </label>
+          <div id={`details${index}`} className="details">
+            <div className="text">
+              <div className="mod-name-background">
+                <a className="mod-name">{mod.modname} <br /> <a href={mod.download}>Download</a></a>
+              </div>
+              <div className="mod-reg-background">
+                <details>
+                  <summary className="regsummary">Mod Requirements</summary>
+                  <ul className="nested-ul">
+                    {mod.requirements.map((req, i) => (
+                      <li key={i}>{req}</li>
+                    ))}
+                  </ul>
+                </details>
+              </div>
+              <div className="mod-dec-background">
+                <details>
+                  <summary className="decsummary">Mod Description</summary>
+                  <div className="dec-background">{mod.description}</div>
+                </details>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ModCircle;
