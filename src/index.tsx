a0b12e6cfc86b8220a1e56164719debf02bf06ef
M3dicCookie <pred102@hotmail.com>
2026-01-11 12:50:23 -0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './Home/Home';
-import ModList from './ModList/ModList';
-import ModderPage from './ModCircle/ModderPage';
-
-import './index.css';import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home/Home';
 import ModderPage from './ModCircle/ModderPage';
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mods/:game/:modder" element={<ModderPage />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-root.render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mods/:game" element={<ModList />} />
-        <Route path="/mods/:game/:Modder" element={<ModderPage />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/mods/:game/:modder" element={<ModderPage />} />
+    </Routes>
+  </BrowserRouter>
 );
