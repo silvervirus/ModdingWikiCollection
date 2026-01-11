650b35d070d6fa589c405a68f155e27984da183c
M3dicCookie <pred102@hotmail.com>
2026-01-11 12:30:25 -0800

    Refactor NavBar component for improved structure

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -1,48 +1,14 @@
-import { Link } from 'react-router-dom';
+import React from 'react';
 import './NavBar.css';
+import { Link } from 'react-router-dom';
 
-export default function NavBar() : JSX.Element {
+export default function NavBar(): JSX.Element {
     return (
-        <>
-            <nav className="navbar">
-                <div><a href="./index.html">Home</a></div>
-                <div>
-                    <a href="#">Subnautica</a>
-                    <ul>
-                        <li><a href="./snhelp.html">Install Instructions</a></li>
-                        <li><Link to="/mods/Legacy">Legacy</Link></li>
-                        <li><Link to="/mods/Stable">LivingLarge</Link></li>
-                        <li><a href="#">TestBuilds</a></li>
-                        
-                    </ul>
-                </div>
-                <div>
-                    <a href="#">BelowZero</a>
-                    <ul>
-                        <li><a href="./bzhelp.html">Install Instructions</a></li>
-                        <li><Link to="/mods/BelowZero-Legacy">Legacy</Link></li>
-                        <li><a href="#">Stable</a></li>
-                        <li><a href="#">TestBuilds</a></li>
-                        
-                    </ul>
-                </div>
-                <div>
-                    <a href="#">Modding Resources</a>
-                    <ul>
-                        <li><a href="#">Legacy</a></li>
-                        <li><a href="#">Stable</a></li>
-                        <li><a href="#">Thunderkit</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <a href="#">Communities</a>
-                    <ul>
-                        <li><a href="#">Modding libraries</a></li>
-                        <li><a href="#">Modding Servers</a></li>
-                        <li><a href="#">Other Modding Resources</a></li>
-                    </ul>
-                </div>
-            </nav>
-        </>
+        <div className="navbar">
+            <div><Link to="/">Home</Link></div>
+            <div><Link to="/mods/Legacy">Legacy Mods</Link></div>
+            <div><Link to="/mods/Stable">Stable Mods</Link></div>
+            <div><Link to="/mods/BelowZero-Legacy">Below Zero Legacy Mods</Link></div>
+        </div>
     );
 }
