a15f09471a9fedfee4da34c1b508d2906d0b6e66
M3dicCookie <pred102@hotmail.com>
2026-01-15 09:59:48 -0800

    Delete public directory

--- public/toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleDetails(id) {
-    var details = document.getElementById(id);
-    if (details.style.display === "none") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
